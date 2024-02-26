70fb84cecfa44e3ab3b2406f098ddf2de357154e
Aniebietabasi <aniebietabasiobo@gmail.com>
2024-02-26 17:16:13 +0100

    mongo live url

--- db/index.js
+++ db/index.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose
-.connect("mongodb://localhost:27017/talebook")
-// .connect(process.env.MONGO_URL)
+.connect(process.env.MONGO_URL)
 .then(
     () => console.log('Database Connected')
 )
